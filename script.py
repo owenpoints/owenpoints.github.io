9297c90b53557cbc7af88cf72933027d65a602ff
owenpoints <thomsonfrankford@duck.com>
2024-11-22 17:11:14 -0500

    Updating Points

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ def output(store):
 
     output_str += "\n## Report Someone or Request Points [Here](https://forms.gle/cc2Y95JU66t6gKew9).\n"
     output_str += "\n## !! Those Under -500 Owen Points will be [Executed Live](https://www.twitch.tv/will_of_owen) !!\n"
-    output_str += "\n## Top Owen Updates Can be Found [Here](https://owenpoints.github.io/blog)\n\n\n## Owen Points Log:\n"
+    output_str += "\n## Top Owen Updates Can be Found [Here](https://owenpoints.github.io/blog).\n\n\n## Owen Points Log:\n"
 
     log = open('log.txt', 'r')
     lines = log.readlines()
