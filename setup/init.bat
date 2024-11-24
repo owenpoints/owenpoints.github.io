8eb85ca1f55581ad5317364bb7cfec1fafbe1291
owenpoints <thomsonfrankford@duck.com>
2024-11-23 19:29:12 -0500

    Automated Site Content Update

--- setup/init.bat
+++ setup/init.bat
@@ -2,7 +2,7 @@
 git init ..\
 git add ..\
 
-set \p bloguser="Input username to use for blog: "
+set /p bloguser="Input username to use for blog: "
 set empty=""
 
 echo %bloguser% > ..\blog\username.txt
@@ -10,15 +10,19 @@ echo {} > ..\blog\store.txt
 echo Run blog.bat to construct your web page. > ..\blog\README.md
 del ..\blog\posts\*.*
 del ..\blog\images\*.*
-del ..\blog\devlog.py
 
 echo %empty% > ..\points\log.txt
 echo {} > ..\points\store.txt
 echo Run points.bat to construct your web page. > ..\README.md
-del ..\favicon.ico
 
-set \p username="Input your GitHub username: "
-set \p repo="Input name of repository: "
+
+set /p username="Input your GitHub username: "
+set /p email="Input the email associated with your GitHub account: "
+set /p repo="Input name of repository: "
+
+git config --global user.email "%email%"
+git config --global user.name "%username%"
+
 git remote add origin https://github.com/%username%/%repo%.git
 
 echo Pushing to GitHub...
